f02b6ae8424a9d166ece5853df095b082a397f41
David <david@zarel.net>
2022-10-21 14:55:12 +0200

    Initial commit: nix support

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "ODALA";
+
+  inputs.nixpkgs.url = "nixpkgs/release-22.05";
+
+  outputs = { self, nixpkgs }: with nixpkgs.legacyPackages.x86_64-linux; {
+    devShell.x86_64-linux = mkShell {
+      buildInputs = [ go_1_18 jq ];
+    };
+  };
+}
